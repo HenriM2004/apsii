671fb2ceec82f570bc49b3eb68e3ffebc03cae39
HENRIQUE MEDEIROS DE ARAUJO <haraujo@cs.unipe.edu.br>
2026-08-20 11:22:48 -0300

    Exercicio02Classe

---
+++ Exercicio02/src/main/java/br/edu/unipe/apsii/Funcionario.java
@@ -0,0 +1,14 @@
+package br.edu.unipe.apsii;
+
+public class Funcionario {
+    public String nome;
+    protected int matricula;
+    protected double salarioBase;
+    protected int dataAdmissao;
+
+    public Funcionario(String nome, int matricula, double salarioBase, int dataAdmissao) {
+        this.nome = funcionarioNome;
+        this.matricula = funcionarioMatric;
+    }
+
+}
---
+++ Exercicio02/src/main/java/br/edu/unipe/apsii/FuncionarioCLT.java
@@ -0,0 +1,18 @@
+package br.edu.unipe.apsii;
+
+public class FuncionarioCLT extends Funcionario{
+
+    public String nome;
+    protected int matricula;
+    protected double salarioBase;
+    protected int dataAdmissao;
+    public String valeTransporte;
+    public String valeAlimentacao;
+
+    public FuncionarioCLT (String nome, int matricuça, int salarioBase, int dataAdmissao, String valeTransporte, String valeAlimentacao) {
+
+        {
+    }
+
+
+}
